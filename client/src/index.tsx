79e1cfb8856e4ea26a0ac154b93932267d43641e
Alex <lepsalex@gmail.com>
2019-01-22 00:00:26 -0500

    Update to use env

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000"
+  uri: process.env.URI || "http://localhost:4000"
 });
 
 ReactDOM.render(
